ac67d1deab12fdeeb48a291e5bee5253a7055983
isaiahwong <isaiah@jirehsoho.com>
2019-07-15 22:54:12 +0800

    Changed http mapping options logic in protoLoader

--- src/utils/grpc.js
+++ src/utils/grpc.js
@@ -1,48 +1,197 @@
-import grpc from 'grpc';
-import logger from 'esther';
-
-import { InternalServerError } from 'horeb';
-
+/* eslint-disable */
 /**
- * Encodes array to Buffer
- * For storing data in grpc.Metadata
- * @param {String} key key that maps to value
- * @param {Array} arr array to be encoded
+ * https://www.npmjs.com/package/@grpc/proto-loader
+ * Forked proto-loader to gain more control over proto objects 
  */
-export function encodeArrayMetadata(key = 'data', arr) {
-  if (!Array.isArray(arr)) {
-    throw new InternalServerError('Arg supplied is not an array');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * @license
+ * Copyright 2018 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+var Protobuf = require("protobufjs");
+var fs = require("fs");
+var path = require("path");
+var _ = require("lodash");
+function joinName(baseName, name) {
+  if (baseName === '') {
+    return name;
+  }
+  else {
+    return baseName + '.' + name;
+  }
+}
+function getAllServices(obj, parentName) {
+  var objName = joinName(parentName, obj.name);
+  if (obj.hasOwnProperty('methods')) {
+    return [[objName, obj]];
+  }
+  else {
+    return obj.nestedArray.map(function (child) {
+      if (child.hasOwnProperty('nested')) {
+        return getAllServices(child, objName);
+      }
+      else {
+        return [];
+      }
+    }).reduce(function (accumulator, currentValue) { return accumulator.concat(currentValue); }, []);
+  }
+}
+function createDeserializer(cls, options) {
+  return function deserialize(argBuf) {
+    return cls.toObject(cls.decode(argBuf), options);
+  };
+}
+function createSerializer(cls) {
+  return function serialize(arg) {
+    var message = cls.fromObject(arg);
+    return cls.encode(message).finish();
+  };
+}
+function createMethodDefinition(method, serviceName, options) {
+  return {
+    path: '/' + serviceName + '/' + method.name,
+    requestStream: !!method.requestStream,
+    responseStream: !!method.responseStream,
+    requestSerialize: createSerializer(method.resolvedRequestType),
+    requestDeserialize: createDeserializer(method.resolvedRequestType, options),
+    responseSerialize: createSerializer(method.resolvedResponseType),
+    responseDeserialize: createDeserializer(method.resolvedResponseType, options),
+    // TODO(murgatroid99): Find a better way to handle this
+    originalName: _.camelCase(method.name),
+    httpEndpoints: mapHttpOptions(method.options),
+    resolvedRequestType: method.resolvedRequestType,
+    resolvedResponseType: method.resolvedResponseType
+  };
+}
+function createServiceDefinition(service, name, options) {
+  var def = {};
+  for (var _i = 0, _a = service.methodsArray; _i < _a.length; _i++) {
+    var method = _a[_i];
+    def[method.name] = createMethodDefinition(method, name, options);
+  }
+  return def;
+}
+function createPackageDefinition(root, options) {
+  var def = {};
+  for (var _i = 0, _a = getAllServices(root, ''); _i < _a.length; _i++) {
+    var _b = _a[_i], name = _b[0], service = _b[1];
+    def[name] = createServiceDefinition(service, name, options);
+  }
+  return def;
+}
+function addIncludePathResolver(root, includePaths) {
+  root.resolvePath = function (origin, target) {
+    for (var _i = 0, includePaths_1 = includePaths; _i < includePaths_1.length; _i++) {
+      var directory = includePaths_1[_i];
+      var fullPath = path.join(directory, target);
+      try {
+        fs.accessSync(fullPath, fs.constants.R_OK);
+        return fullPath;
+      }
+      catch (err) {
+        continue;
+      }
+    }
+    return null;
+  };
+}
+/**
+ * @author isaiahwong
+ * @param {*} options 
+ */
+function mapHttpOptions(options) {
+  if (!options) {
+    return;
+  }
+  const httpMethods = {
+    get: 'get',
+    put: 'put',
+    post: 'post',
+    delete: 'delete',
+    patch: 'patch',
+  }
+  const httpOptions = Object.keys(options).reduce((accum, key) => {
+    if (!key.match('http.')) {
+      return accum;
+    }
+    const method = httpMethods[key.replace('http.', '')];
+    if (method) {
+      accum.path = options[key];
+      accum.method = method;
+    }
+    if (key.match('body')) {
+      accum.body = options[key];
+    }
+
+    return accum;
+
+  }, {})
+  return _.isEmpty(httpOptions) ? null : httpOptions;
+}
+/**
+ * Load a .proto file with the specified options.
+ * @param filename The file path to load. Can be an absolute path or relative to
+ *     an include path.
+ * @param options.keepCase Preserve field names. The default is to change them
+ *     to camel case.
+ * @param options.longs The type that should be used to represent `long` values.
+ *     Valid options are `Number` and `String`. Defaults to a `Long` object type
+ *     from a library.
+ * @param options.enums The type that should be used to represent `enum` values.
+ *     The only valid option is `String`. Defaults to the numeric value.
+ * @param options.bytes The type that should be used to represent `bytes`
+ *     values. Valid options are `Array` and `String`. The default is to use
+ *     `Buffer`.
+ * @param options.defaults Set default values on output objects. Defaults to
+ *     `false`.
+ * @param options.arrays Set empty arrays for missing array values even if
+ *     `defaults` is `false`. Defaults to `false`.
+ * @param options.objects Set empty objects for missing object values even if
+ *     `defaults` is `false`. Defaults to `false`.
+ * @param options.oneofs Set virtual oneof properties to the present field's
+ *     name
+ * @param options.includeDirs Paths to search for imported `.proto` files.
+ */
+function load(filename, options) {
+  var root = new Protobuf.Root();
+  if (!!options.includeDirs) {
+    if (!(options.includeDirs instanceof Array)) {
+      return Promise.reject(new Error('The includeDirs option must be an array'));
+    }
+    addIncludePathResolver(root, options.includeDirs);
   }
 
-  const metadata = new grpc.Metadata();
-  arr.forEach((detail, i) => {
-    if (i) {
-      metadata.add(`${key}-bin`, Buffer.from(JSON.stringify(detail)));
-    }
-    else {
-      metadata.set(`${key}-bin`, Buffer.from(JSON.stringify(detail)));
-    }
+  return root.load(filename, options).then(function (loadedRoot) {
+    loadedRoot.resolveAll();
+    return createPackageDefinition(root, options);
   });
-
-  return metadata;
 }
-
-/**
- * Decodes array to Buffer
- * For storing data in grpc.Metadata
- * @param {String} key that maps to value
- * @param {grpc.Metadata} metadata to be decoded
- */
-export function decodeArrayMetadata(key = 'data', metadata) {
-  if (!(metadata instanceof grpc.Metadata)) {
-    throw new InternalServerError('Arg supplied is not of type grpc.Metadata');
+exports.load = load;
+function loadSync(filename, options) {
+  var root = new Protobuf.Root();
+  if (!!options.includeDirs) {
+    if (!(options.includeDirs instanceof Array)) {
+      throw new Error('The include option must be an array');
+    }
+    addIncludePathResolver(root, options.includeDirs);
   }
-
-  const details = metadata.get(`${key}-bin`);
-  if (!Array.isArray(details)) {
-    logger.warn('key value is not an array');
-    return [];
-  }
-
-  return details.map(detail => JSON.parse(detail.toString()));
+  var loadedRoot = root.loadSync(filename, options);
+  loadedRoot.resolveAll();
+  return createPackageDefinition(root, options);
 }
+exports.loadSync = loadSync;
+//# sourceMappingURL=index.js.map
